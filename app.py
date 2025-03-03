24b36c690f92ef0818fc4d03a68bf248a1a1d981
JITHNU <jithnukaweerasingha@gmail.com>
2025-03-04 00:23:47 +0530

    Fixed Flask port for Railway

--- app.py
+++ app.py
@@ -55,5 +55,5 @@ def predict():
 
 if __name__ == '__main__':
     # Use Railway's assigned port dynamically
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=True)
